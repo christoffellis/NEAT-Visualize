5151be873420d2b07e3919f2a7beba4a775df27b
christoffellis <30555044+christoffellis@users.noreply.github.com>
2022-12-07 22:09:34 +0200

    Updated
    Nodes now appear in order instead of all on top of each other Reworked colour as to use anti-aliasing lines. Now colour indicates strength instead of line width Added the ability to add the node names for input nodes

--- NEATvisualize.py
+++ NEATvisualize.py
@@ -1,21 +1,8 @@
 import pygame
 
 
-def drawNetwork(config, genome, size, node_names=None, node_colors=None, show_disabled=True):
+def drawNetwork(config, genome, size, node_names=None, show_disabled=True):
     screen = pygame.Surface((size))
-
-    if node_names is None:
-        node_names = {}
-
-    assert type(node_names) is dict
-
-    if node_colors is None:
-        node_colors = {}
-
-    assert type(node_colors) is dict
-
-
-    r = min(size[0] // 20, size[1] // 20)
 
     inputs = []
     for x, k in enumerate(config.genome_config.input_keys):
@@ -25,42 +12,73 @@ def drawNetwork(config, genome, size, node_names=None, node_colors=None, show_di
     for x, k in enumerate(config.genome_config.output_keys):
         outputs.append(str(k))
 
+
+    r = min(size[0] // 30, size[1] // 30, size[0] // (len(inputs) * 5), size[1] // (len(outputs) * 5))
+
+
     hidden_keys = []
     for n in genome.nodes.keys():
         if str(n) in inputs or str(n) in outputs:
             continue
 
-        hidden_keys.append(str(n))
+        hidden_keys.append([str(n), 0])
+
+    maxLayers = 0
+    if len(hidden_keys) > 0:
+        for x in range(len(hidden_keys)):
+            hidden_keys[x][1] = get_dependency_count(hidden_keys[x], genome, outputs, hidden_keys, show_disabled)
+            maxLayers = max(maxLayers, hidden_keys[x][1])
 
     for cg in genome.connections.values():
         if cg.enabled or show_disabled:
             input, output = cg.key
-            a = node_names.get(input, str(input))
-            b = node_names.get(output, str(output))
-            color = 'green' if cg.weight > 0 else 'red' if cg.enabled else 'gray'
-            width = 1 + abs(cg.weight)
-            aPos = get_position(a, inputs, outputs, hidden_keys, size, r)
-            bPos = get_position(b, inputs, outputs, hidden_keys, size, r)
+            a = str(input)
+            b = str(output)
+            widthColour = min(127, max(0, (cg.weight / 4) * 127))
+            color = (127 - widthColour, 127 + widthColour, 0) if cg.enabled else 'gray'
+            aPos = get_position(a, inputs, outputs, hidden_keys, size, r, maxLayers)
+            bPos = get_position(b, inputs, outputs, hidden_keys, size, r, maxLayers)
 
-            pygame.draw.line(screen, color, aPos, bPos, width=int(width))
+            pygame.draw.aaline(screen, color, aPos, bPos) #, width=int(width))
+
+
 
     offset = size[1] // len(config.genome_config.input_keys)
     for x in range(len(inputs)):
         pygame.draw.circle(screen, (255, 255, 255), (r, offset * x + offset // 2), r)
+        if node_names is not None:
+            writeText(screen, node_names[0][x], 0, offset * x + offset // 2 - r * 1.2, (255, 255, 255), True)
+
 
     offset = size[1] // len (config.genome_config.output_keys)
     for x in range(len(outputs)):
         pygame.draw.circle(screen, (255, 255, 255), (size[0]-r, offset * x + offset // 2), r)
 
     if len(hidden_keys) > 0:
-        offset = size[1] // len(hidden_keys)
+        offsetY = size[1] // len(hidden_keys)
+
+        offsetX = size[0] // (maxLayers + 1)
+
         for x, k in enumerate(hidden_keys):
-            pygame.draw.circle(screen, (255, 255, 255), (size[0] // 2, offset * x + offset // 2), r)
+            pygame.draw.circle(screen, (255, 255, 255), ((maxLayers + 1 - k[1]) * offsetX, offsetY * x + offsetY // 2), r)
 
     return screen
 
 
-def get_position(key, inputs, outputs, hidden, size, r):
+def get_dependency_count(id, genome, outputs, hidden, show_disabled):
+    maxOut = 0
+    for cg in genome.connections.values():
+        if cg.enabled or show_disabled:
+            inputted, output = cg.key
+            if id[0] == str(inputted):
+                if str(output) in outputs:
+                    maxOut = max(1, maxOut)
+                else:
+                    maxOut = max(get_dependency_count([str(output), 0], genome, outputs, hidden, show_disabled) + 1, maxOut)
+    return maxOut
+
+
+def get_position(key, inputs, outputs, hidden, size, r, maxLayers):
     if key in inputs:
         offset = size[1] // len(inputs)
         return (r, offset * inputs.index(key) + offset // 2)
@@ -69,6 +87,29 @@ def get_position(key, inputs, outputs, hidden, size, r):
         offset = size[1] // len(outputs)
         return (size[0] - r, offset * outputs.index(key) + offset // 2)
 
-    if key in hidden:
-        offset = size[1] // len(hidden)
-        return (size[0] // 2, offset * hidden.index(key) + offset // 2)
+    for x, k in enumerate(hidden):
+        if key == k[0]:
+
+            offsetY = size[1] // len(hidden)
+            offsetX = size[0] // (maxLayers + 1)
+
+            return ((maxLayers + 1 - k[1]) * offsetX, offsetY * x + offsetY // 2)
+
+
+
+def writeText(screen, string, coordx, coordy, colour, center=True, background=None):
+    # set the font to write with
+    font = pygame.font.SysFont('calibri', 10)
+    if background == None:
+        # (0, 0, 0) is black, to make black text
+        text = font.render(string, True, colour)
+    else:
+        text = font.render(string, True, colour, background)
+    # get the rect of the text
+    textRect = text.get_rect()
+    # set the position of the text
+    if center:
+        textRect.bottomleft = (coordx, coordy)
+    else:
+        textRect.topleft = (coordx, coordy)
+    screen.blit(text, textRect)
